ed2f92df710af5956ba12afee0853f73088bc204
Mugilan Ganesan <49064156+MugilanGN@users.noreply.github.com>
2020-07-26 12:41:02 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ultra_sockets",
+    version="1.0.0",
+    author="Mugilan Ganesan",
+    author_email="mugi.ganesan@gmail.com",
+    description="Protocol to provide fast and light communication between devices over (W)LAN",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MugilanGN/UltraSockets",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    python_requires='>=3.4',
+)
